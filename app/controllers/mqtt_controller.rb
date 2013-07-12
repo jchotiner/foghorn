62fd7d682cc618d4c7418c3109248610a27599a8
Jeremy Chotiner <jeremy.chotiner@tapjoy.com>
2013-07-12 14:36:07 -0400

    some style some function

---
+++ app/controllers/mqtt_controller.rb
@@ -0,0 +1,19 @@
+class MqttController < ApplicationController
+
+  def publish
+    uri = URI.parse ENV['CLOUDMQTT_URL'] || 'mqtt://localhost:1883'
+    conn_opts = {
+    remote_host: uri.host,
+    remote_port: uri.port,
+    username: uri.user,
+    password: uri.password,
+    }
+    MQTT::Client.connect(conn_opts) do |c|
+      c.publish(params[:topic], params[:payload])
+    end
+  end
+
+  def home
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Foghorn::Application.routes.draw do
+   get 'mqtt/publish' => 'mqtt#publish'
+   root 'mqtt#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +41,7 @@ Foghorn::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
